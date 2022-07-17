a87d0e8fe0f2660c3155b9ed0ded7256a49ac932
raeyoung-kim <760kry@gmail.com>
2022-07-18 02:58:48 +0900

    헤더 유저 로그아웃 수정

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import { IoChatbubblesSharp } from 'react-icons/io5';
 import serverAPI from 'services/api';
+import { AiFillCaretDown } from 'react-icons/ai';
 import styles from './Header.module.css';
-
 interface Props {
   user: User;
 }
 
 const Header: React.FC<Props> = ({ user }) => {
   const { reload } = useRouter();
+  const [isMenu, setIsMenu] = useState(false);
 
   const handleLogout = async () => {
     try {
@@ -28,11 +29,30 @@ const Header: React.FC<Props> = ({ user }) => {
             <IoChatbubblesSharp className={styles.icon} />
           </a>
         </Link>
-        <div>
+        <div style={{ height: '100%' }}>
           {user.userId ? (
-            <button className={styles.button} onClick={handleLogout}>
-              로그아웃
-            </button>
+            <div className={styles.menu}>
+              <div
+                style={{
+                  backgroundColor: isMenu ? 'rgba(0,0,0,.05)' : 'white',
+                }}
+                className={styles.user}
+                onClick={() => setIsMenu(!isMenu)}
+              >
+                {user.name} <AiFillCaretDown className={styles.caret_down} />
+              </div>
+              <div
+                style={{
+                  position: 'relative',
+                }}
+              >
+                {isMenu && (
+                  <ul className={styles.menu_list}>
+                    <li onClick={handleLogout}>로그아웃</li>
+                  </ul>
+                )}
+              </div>
+            </div>
           ) : (
             <Link href={'/sign-in'}>
               <a className={styles.button}>로그인</a>
